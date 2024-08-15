7bf296f713e2f054e7d5b8b1e81f3d52c54790d7
Halil-KAPLAN <halil-kaplan@windowslive.com>
2024-08-15 18:06:18 +0300

    Add project files.

---
+++ MyCar_Creation/Model/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCar_Creation.Model
+{
+    public abstract class BaseModel
+    {
+        protected BaseModel()
+        {
+            Id = Guid.NewGuid();
+            CreadtedDate = DateTime.Now;
+
+        }
+
+        public Guid Id { get; set; }
+        public DateTime CreadtedDate { get; set; }
+    }
+}
